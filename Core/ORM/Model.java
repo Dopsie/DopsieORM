7fd2f81f566ab4d5d4ba222c2ec449c298867d64
Amine Troudi <aminetroudi@think-it.io>
2018-03-26 19:07:40 +0100

    Model docs

--- Core/ORM/Model.java
+++ Core/ORM/Model.java
@@ -14,35 +14,62 @@ import java.util.Map;
  * Model
  */
 public class Model extends RelationalModel {
-
+    // ConditionStack containing the list of conditions from the built query.
     private ArrayList<ArrayList<String>> conditionStack;
 
+    /**
+     * Initializes the newly created Model instance : 
+     *  setting the state of instance to new. (To be inserted into the database later)
+     *  initializes the conditionStack and the attributes with empty lists.
+     */
     public Model() {
         super();
         this.isNew = true;
         conditionStack = new ArrayList<ArrayList<String>>();
         attributes = new HashMap<String, Object>();
     }
-
+    /**
+     * @return A hashmap of attributes name => value 
+     */
     public HashMap<String, Object> getAllAttributes() {
         return this.attributes;
     }
 
+    /**
+     * Get the value of an attribute by name.
+     * @param columnName the name of the attribute.
+     * @return Object containing the value of the requested attribute.
+     */
     public Object getAttr(String columnName) {
         return this.attributes.get(columnName);
     }
 
+    /**
+     * Set value of an attribute
+     * @param columnName the name of the attribute to be updated or added.
+     * @param value the value of the attribute
+     */
     public void setAttr(String columnName, Object value) {
         this.isModified = true;
         this.attributes.put(columnName, value);
     }
 
+    /**
+     * Add a where condition to the conditionStack.
+     * @param arg1 The name of the column that will be matched
+     * @param arg2 The searched value.
+     * @param operator The comparaison operator.
+
+     */
     public Model where(String arg1, String operator, String arg2) {
         ArrayList<String> condition = new ArrayList<String>(Arrays.asList(arg1, operator, arg2));
         conditionStack.add(condition);
         return this;
     }
 
+    /**
+     * The Model to be fetched.
+     */
     public static <T> T fetch(Class<T> theClass) throws ModelException {
         try {
             return theClass.cast(theClass.newInstance());
@@ -50,11 +77,18 @@ public class Model extends RelationalModel {
             throw new ModelException("Couldn't create Model");
         }
     }
-
+    /**
+     * Get the model as it is to start building the query. 
+     */
     public Model all() {
         return this;
     }
 
+    /**
+     * Find an entry by primaryKey 
+     * @param theClass The Model class from where the data will be fetched.
+     * @param id the value of the primaryKey 
+     */
     public static <T extends Model> T find(Class<T> theClass, int id) throws ModelException {
         try {
             // Model instanciated in order to get primary Key's column name
@@ -66,7 +100,9 @@ public class Model extends RelationalModel {
         }
     }
 
-    // retrieving data from a table 
+    /**
+     * Build the query from the condition stack.
+     */
     public ArrayList execute() throws UnsupportedDataTypeException, ModelException {
         ArrayList args = new ArrayList();
         String query = "SELECT * FROM " + this.getTableName();
